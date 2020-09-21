72bf2b322441945dd2341e6ce3155026713e31a0
dradxxx@gmail.com <ygolb@mail.ru>
2020-09-21 20:01:03 +0500

    lesson 6

--- src/main/java/org/example/web/controllers/BookShelfController.java
+++ src/main/java/org/example/web/controllers/BookShelfController.java
@@ -12,8 +12,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 @Controller
 @RequestMapping(value = "/books")
@@ -61,5 +67,27 @@ public class BookShelfController {
             bookService.removeBookById(bookIdToRemove.getId());
             return "redirect:/books/shelf";
         }
+    }
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestParam("file") MultipartFile file) throws Exception {
+        String name = file.getOriginalFilename();
+        byte[] bytes = file.getBytes();
+        //create dir
+        String rootPath = System.getProperty("catalina.home");
+        File dir = new File(rootPath + File.separator + "external_uploads");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        //create file
+        File serverFile = new File(dir.getAbsolutePath() + File.separator + name);
+        try (BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile))) {
+            stream.write(bytes);
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+        }
+        logger.info("new file saved at: " + serverFile.getAbsolutePath());
+        return "redirect:/books/shelf";
     }
 }
